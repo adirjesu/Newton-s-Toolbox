c92e7b2df91e2020307fddc709eedfa18697dab0
Adair <adairjesus.cruz@gmail.com>
2025-05-27 11:04:44 -0600

    Prueba1

---
+++ app/src/main/java/com/example/newtoons_toolbox/ACT_F1_Leydehooke.java
@@ -0,0 +1,56 @@
+package com.example.newtoons_toolbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class ACT_F1_Leydehooke extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_f1_leydehooke);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menuestudiantes,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
+        int id=item.getItemId();
+        if(id==R.id.regresar){
+            Toast.makeText(this, "Regresando",
+                    Toast.LENGTH_SHORT).show();
+            Intent w =  new Intent(this,ACT_LobbyFisica1.class);
+            startActivity(w);
+        }
+
+        if(id==R.id.Algebra){
+            Toast.makeText(this, "Accediendo a Algebra",
+                    Toast.LENGTH_SHORT).show();
+            Intent x =  new Intent(this,ACT_LobbyAlgebra.class);
+            startActivity(x);
+        }
+
+        if(id==R.id.Fisica1){
+            Toast.makeText(this, "Accediendo a Física I",
+                    Toast.LENGTH_SHORT).show();
+            Intent y =  new Intent(this,ACT_LobbyFisica1.class);
+            startActivity(y);
+        }
+        if(id==R.id.Fisica2){
+            Toast.makeText(this, "Accediendo a Física II",
+                    Toast.LENGTH_SHORT).show();
+            Intent z =  new Intent(this,ACT_LobbyFisica2.class);
+            startActivity(z);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/newtoons_toolbox/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.newtoons_toolbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public TextView titulo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        titulo=findViewById(R.id.txtTitulo);
+        titulo.setGravity(Gravity.HORIZONTAL_GRAVITY_MASK);
+
+    }
+    public void ocInicio (View view){
+        Intent x =  new Intent(this,ACT_Login.class);
+        startActivity(x);
+    }
+}
