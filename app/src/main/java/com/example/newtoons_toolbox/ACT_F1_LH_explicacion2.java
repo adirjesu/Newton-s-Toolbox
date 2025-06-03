8bf164e3d8dc1e8bc6c1bb68e9658db0d224b4fa
Adair <adairjesus.cruz@gmail.com>
2025-06-03 11:43:43 -0600

    Se añadieron archivos de explicacion

---
+++ app/src/main/java/com/example/newtoons_toolbox/ACT_F1_LH_explicacion2.java
@@ -0,0 +1,14 @@
+package com.example.newtoons_toolbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ACT_F1_LH_explicacion2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_f1_lh_explicacion2);
+    }
+}
