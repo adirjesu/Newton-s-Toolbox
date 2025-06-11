b4a40c85c671a848859d032949b25194fe375f5d
Luis <luisfgns@gmail.com>
2025-06-11 08:56:01 -0600

    Imagen del looby del estudiante centrada y se modificó el lobby de Física 1 para tener solo 3 temas, así como el backend

--- app/src/main/java/com/example/newtoons_toolbox/ACT_LobbyFisica1.java
+++ app/src/main/java/com/example/newtoons_toolbox/ACT_LobbyFisica1.java
@@ -12,27 +12,28 @@ import android.widget.Toast;
 
 public class ACT_LobbyFisica1 extends AppCompatActivity {
     private Button hidro;
-    private Button torricelli;
+    //private Button torricelli;
     private Button pascal;
-    private Button hidrostatica;
+    //private Button hidrostatica;
     private Button hooke;
-    private Button arqui;
+    //private Button arqui;
     private Button young;
     public static int tema_elegido=-1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_lobby_fisica1);
-        hidro=findViewById(R.id.btnhidrodinamica);
-        hidro=findViewById(R.id.btntorricelli);
+        //hidro=findViewById(R.id.btnhidrodinamica);
+        //hidro=findViewById(R.id.btntorricelli);
         hidro=findViewById(R.id.btnpascal);
-        hidro=findViewById(R.id.btnhidrostatica);
+        //hidro=findViewById(R.id.btnhidrostatica);
         hidro=findViewById(R.id.btnhooke);
-        hidro=findViewById(R.id.btnarquimidez);
+        //hidro=findViewById(R.id.btnarquimidez);
         hidro=findViewById(R.id.btnyoung);
 
     }//main
-    public void ocHidrodinamica (View view){
+
+    /*public void ocHidrodinamica (View view){
         tema_elegido=1;
         Bundle enviarDatos = new Bundle();
         enviarDatos.putInt("tema",tema_elegido);
@@ -47,7 +48,7 @@ public class ACT_LobbyFisica1 extends AppCompatActivity {
         Intent x =  new Intent(this,ACT_F1_TemaLobby.class);
         x.putExtras(enviarDatos);
         startActivity(x);
-    }
+    }*/
     public void ocPascal (View view){
         tema_elegido=3;
         Bundle enviarDatos = new Bundle();
@@ -56,14 +57,14 @@ public class ACT_LobbyFisica1 extends AppCompatActivity {
         x.putExtras(enviarDatos);
         startActivity(x);
     }
-    public void ocHidrostatica (View view){
+    /*public void ocHidrostatica (View view){
         tema_elegido=4;
         Bundle enviarDatos = new Bundle();
         enviarDatos.putInt("tema",tema_elegido);
         Intent x =  new Intent(this,ACT_F1_TemaLobby.class);
         x.putExtras(enviarDatos);
         startActivity(x);
-    }
+    }*/
     public void ocHooke (View view){
         tema_elegido=5;
         Bundle enviarDatos = new Bundle();
@@ -71,14 +72,16 @@ public class ACT_LobbyFisica1 extends AppCompatActivity {
         Intent x =  new Intent(this,ACT_F1_TemaLobby.class);
         x.putExtras(enviarDatos);
         startActivity(x);
-    }    public void ocArquimidez (View view){
+    }
+    /*public void ocArquimidez (View view){
         tema_elegido=6;
         Bundle enviarDatos = new Bundle();
         enviarDatos.putInt("tema",tema_elegido);
         Intent x =  new Intent(this,ACT_F1_TemaLobby.class);
         x.putExtras(enviarDatos);
         startActivity(x);
-    }    public void ocYoung (View view){
+    }    */
+    public void ocYoung (View view){
         tema_elegido=7;
         Bundle enviarDatos = new Bundle();
         enviarDatos.putInt("tema",tema_elegido);
